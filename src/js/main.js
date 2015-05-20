b08172f1ceb02e437aaa63a1779f2dc13a7ca121
Bryan Duplantis <duplantis@gmail.com>
2015-05-20 15:54:44 -0500

    new files

--- src/js/main.js
+++ src/js/main.js
@@ -1,84 +1,96 @@
 var FIREBASE_URL = 'https://nssc9authapp.firebaseio.com/';
 var fb = new Firebase(FIREBASE_URL);
-var onLoggedOut = $('.onLoggedOut');
-var onLoggedIn = $('.onLoggedIn');
 
-var loginPage = $('.login');
+$('.doResetPassword').click(function () {
+  var email = $('input[type="email"]').val();
 
-var registerPage = $('.register');
-
-$('.toLoginBtn, .toRegisterBtn').click(toggleLoginRegister);
-
-$('.doLogout').click(function () {
-  fb.unauth();
-  toggleContentBasedOnLogin();
-})
-
-$('.register form').submit(function () {
-  var email = $('.register input[type="email"]');
-  var passwords = $('.register input[type="password"]');
-  var password = $(passwords[0]).val();
-  var passwordCheck = $(passwords[1]).val();
-
-  if (password === passwordCheck) {
-    fb.createUser({
-      email: email.val(),
-      password: password
-    },function (err, userData) {
-      if (err) {
-        console.log(err.toString());
-      } else {
-        toggleLoginRegister();
-      }
-    });
-  } else {
-    alert('The passwords do not match');
-  }
-
-  event.preventDefault();
-});
-
-$('.login form').submit(function () {
-  var email = $('.login input[type="email"]');
-  var password = $('.login input[type="password"]');
-
-  fb.authWithPassword({
-    email: email.val(),
-    password: password.val()
-  }, function (err, authData) {
+  fb.resetPassword({
+    email: email
+  }, function (err) {
     if (err) {
       alert(err.toString());
     } else {
-      toggleContentBasedOnLogin();
-      var h1 = $('.onLoggedIn h1').text(`Hello ${authData.password.email}`)
-      email.val('');
-      password.val('');
-      $.ajax({
-        method: 'PUT',
-        url: `${FIREBASE_URL}/users/${authData.uid}/profile.json`,
-        data: JSON.stringify(authData)
-      });
+      alert('Check your email!');
+    }
+  });
+});
+
+$('.doLogout').click(function () {
+  fb.unauth();
+})
+
+$('.doRegister').click(function () {
+  var email = $('input[type="email"]').val();
+  var password = $('input[type="password"]').val();
+
+  fb.createUser({
+    email: email,
+    password: password
+  }, function (err, userData) {
+    if (err) {
+      alert(err.toString());
+    } else {
+      doLogin(email, password);
     }
   });
 
   event.preventDefault();
 });
 
-toggleContentBasedOnLogin();
+$('form').submit(function () {
+  var email = $('input[type="email"]').val();
+  var password = $('input[type="password"]').val();
 
-function toggleLoginRegister() {
-  registerPage.toggleClass('hidden');
-  loginPage.toggleClass('hidden');
+  doLogin(email, password);
+  event.preventDefault();
+});
+
+function clearLoginForm () {
+  $('input[type="email"]').val('');
+  $('input[type="password"]').val('');
 }
 
-function toggleContentBasedOnLogin() {
-  var authData = fb.getAuth();
+function saveAuthData (authData) {
+  $.ajax({
+    method: 'PUT',
+    url: `${FIREBASE_URL}/users/${authData.uid}/profile.json`,
+    data: JSON.stringify(authData)
+  });
+}
 
-  if (authData) {
+function doLogin (email, password, cb) {
+  fb.authWithPassword({
+    email: email,
+    password: password
+  }, function (err, authData) {
+    if (err) {
+      alert(err.toString());
+    } else {
+      saveAuthData(authData);
+      typeof cb === 'function' && cb(authData);
+    }
+  });
+}
+
+fb.onAuth(function (authData) {
+  var onLoggedOut = $('.onLoggedOut');
+  var onLoggedIn = $('.onLoggedIn');
+  var onTempPassword = $('.onTempPassword');
+
+  if (authData && authData.password.isTemporaryPassword) {
+    onTempPassword.removeClass('hidden');
+    onLoggedIn.addClass('hidden');
     onLoggedOut.addClass('hidden');
+  } else if (authData) {
     onLoggedIn.removeClass('hidden');
+    onLoggedOut.addClass('hidden');
+    onTempPassword.addClass('hidden');
+    $('.onLoggedIn h1').text(`Hello ${authData.password.email}`);
   } else {
     onLoggedOut.removeClass('hidden');
     onLoggedIn.addClass('hidden');
+    onTempPassword.addClass('hidden');
   }
-}
+
+  clearLoginForm();
+});
